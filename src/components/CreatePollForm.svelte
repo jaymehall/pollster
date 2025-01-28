bc59d197f3ba3a35241aabbf4c9a6843edfed95e
theprivacydev <theprivacydev@protonmail.com>
2025-01-28 15:49:53 -0600

    created validation for poll form which works successfull

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -2,9 +2,40 @@
     import Button from "../shared/Button.svelte";
 
     let fields = {question: "", answerA: "", answerB: ""}
+    let errors = {...fields};
+    const fieldRequiredMsg = "You must fill out this field";
+    const validateForm = () => {
+        let isValid = true;
+        // validate question
+        if(fields.question.trim().length < 5) {
+            isValid = false;
+            errors.question = "Question must be at least 5 characters long"
+        } else {
+            errors.question = "";
+        }
 
+        //validate answerA
+        if(fields.answerA.trim().length === 0) {
+            isValid = false;
+            errors.answerA = fieldRequiredMsg;
+        } else {
+            errors.answerA = "";
+        }
+
+        //validate answerB
+        if(fields.answerB.trim().length === 0) {
+            isValid = false;
+            errors.answerB = fieldRequiredMsg;
+        } else {
+            errors.answerB = "";
+        }
+
+        return isValid
+    }
     const submitHandler = () => {
-        console.log(fields);
+       const isValid = validateForm();
+       if(!isValid) return;
+
     }
 </script>
 
@@ -12,14 +43,17 @@
     <div class="form-field">
         <label for="question">Poll Question:</label>
         <input type="text" id="question" bind:value={fields.question}/>
+        <div class="error">{errors.question}</div>
     </div>
     <div class="form-field">
         <label for="answer-a">Answer A:</label>
         <input type="text" id="question" bind:value={fields.answerA}/>
+        <div class="error">{errors.answerA}</div>
     </div>
     <div class="form-field">
         <label for="answer-b">Answer B:</label>
         <input type="text" id="question" bind:value={fields.answerB}/>
+        <div class="error">{errors.answerB}</div>
     </div>
     <Button type="primary" flat={true} inverse={true}>Add Poll</Button>
 </form>
@@ -39,6 +73,11 @@
     }
     label{
         margin: 10px auto;
+        text-align: left;
+    }
+    .error{
+        color: red;
+        font-size: small;
         text-align: left;
     }
 </style>
